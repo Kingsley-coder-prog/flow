d17dc2d1a0cb4b9b55725d62f654adf251a91f7c
BlockGem <kifeanyi216@gmail.com>
2024-10-06 09:10:27 +0100

    Watchers & Compute

--- src/components/FlowGraph.vue
+++ src/components/FlowGraph.vue
@@ -1,35 +1,52 @@
 <template>
-    <div>
-      <div class="nav">
-        <button id="triggers" @click="loadTriggers" :class="{'navactive': activeNav === 'triggers'}">Triggers</button>
-        <button id="actions" @click="loadActions" :class="{'navactive': activeNav === 'actions'}">Actions</button>
-      </div>
-  
-      <div id="blocklist" v-html="blockList"></div>
-      <div id="canvas"></div>
+  <div>
+    <div class="nav">
+      <button
+        id="triggers"
+        @click="loadTriggers"
+        :class="{ navactive: activeNav === 'triggers' }"
+      >
+        Triggers
+      </button>
+      <button
+        id="actions"
+        @click="loadActions"
+        :class="{ navactive: activeNav === 'actions' }"
+      >
+        Actions
+      </button>
     </div>
-  </template>
+
+    <div id="blocklist" v-html="blockList"></div>
+    <div id="canvas"></div>
+  </div>
+</template>
   
   <script>
-  export default {
-    data() {
-      return {
-        activeNav: 'triggers',
-        blockList: '',
-      };
+export default {
+  data() {
+    return {
+      activeNav: "triggers",
+      blockList: "",
+    };
+  },
+  mounted() {
+    this.loadTriggers();
+    this.initializeFlowy();
+  },
+  methods: {
+    initializeFlowy() {
+      // Initialize Flowy and set drag, release, snapping handlers
+      flowy(
+        document.getElementById("canvas"),
+        this.drag,
+        this.release,
+        this.snapping
+      );
     },
-    mounted() {
-      this.loadTriggers();
-      this.initializeFlowy();
-    },
-    methods: {
-      initializeFlowy() {
-        // Initialize Flowy and set drag, release, snapping handlers
-        flowy(document.getElementById("canvas"), this.drag, this.release, this.snapping);
-      },
-      loadTriggers() {
-        this.activeNav = 'triggers';
-        this.blockList = `
+    loadTriggers() {
+      this.activeNav = "triggers";
+      this.blockList = `
           <div class="blockelem create-flowy noselect">
             <input type="hidden" name="blockelemtype" class="blockelemtype" value="1">
             <div class="grabme"><img src="assets/grabme.svg"></div>
@@ -52,10 +69,10 @@
               </div>
             </div>
           </div>`;
-      },
-      loadActions() {
-        this.activeNav = 'actions';
-        this.blockList = `
+    },
+    loadActions() {
+      this.activeNav = "actions";
+      this.blockList = `
           <div class="blockelem create-flowy noselect">
             <input type="hidden" name="blockelemtype" class="blockelemtype" value="5">
             <div class="grabme"><img src="assets/grabme.svg"></div>
@@ -78,28 +95,28 @@
               </div>
             </div>
           </div>`;
-      },
-      drag(block) {
-        block.classList.add("blockdisabled");
-      },
-      release() {
-        const block = document.querySelector(".blockdisabled");
-        if (block) {
-          block.classList.remove("blockdisabled");
-        }
-      },
-      snapping(drag, first) {
-        const grab = drag.querySelector(".grabme");
-        grab.parentNode.removeChild(grab);
-        const blockin = drag.querySelector(".blockin");
-        blockin.parentNode.removeChild(blockin);
-  
-        const blockType = drag.querySelector(".blockelemtype").value;
-        let blockContent = '';
-        
-        switch (blockType) {
-          case "1":
-            blockContent = `
+    },
+    drag(block) {
+      block.classList.add("blockdisabled");
+    },
+    release() {
+      const block = document.querySelector(".blockdisabled");
+      if (block) {
+        block.classList.remove("blockdisabled");
+      }
+    },
+    snapping(drag, first) {
+      const grab = drag.querySelector(".grabme");
+      grab.parentNode.removeChild(grab);
+      const blockin = drag.querySelector(".blockin");
+      blockin.parentNode.removeChild(blockin);
+
+      const blockType = drag.querySelector(".blockelemtype").value;
+      let blockContent = "";
+
+      switch (blockType) {
+        case "1":
+          blockContent = `
               <div class='blockyleft'>
                 <img src='assets/eyeblue.svg'>
                 <p class='blockyname'>New visitor</p>
@@ -109,9 +126,9 @@
               </div>
               <div class='blockydiv'></div>
               <div class='blockyinfo'>When a <span>new visitor</span> goes to <span>Site 1</span></div>`;
-            break;
-          case "2":
-            blockContent = `
+          break;
+        case "2":
+          blockContent = `
               <div class='blockyleft'>
                 <img src='assets/actionblue.svg'>
                 <p class='blockyname'>Action is performed</p>
@@ -121,9 +138,9 @@
               </div>
               <div class='blockydiv'></div>
               <div class='blockyinfo'>When <span>Action 1</span> is performed</div>`;
-            break;
-          case "5":
-            blockContent = `
+          break;
+        case "5":
+          blockContent = `
               <div class='blockyleft'>
                 <img src='assets/databaseorange.svg'>
                 <p class='blockyname'>New database entry</p>
@@ -133,9 +150,9 @@
               </div>
               <div class='blockydiv'></div>
               <div class='blockyinfo'>Add <span>Data object</span> to <span>Database 1</span></div>`;
-            break;
-          case "6":
-            blockContent = `
+          break;
+        case "6":
+          blockContent = `
               <div class='blockyleft'>
                 <img src='assets/databaseorange.svg'>
                 <p class='blockyname'>Update database</p>
@@ -145,18 +162,19 @@
               </div>
               <div class='blockydiv'></div>
               <div class='blockyinfo'>Update <span>Database 1</span></div>`;
-            break;
-          // Add more cases as needed...
-        }
-  
-        drag.innerHTML += blockContent;
-        return true;
+          break;
+        // Add more cases as needed...
       }
-    }
-  };
-  </script>
+
+      drag.innerHTML += blockContent;
+      return true;
+    },
+  },
+};
+</script>
   
   <style scoped>
-  /* Add your styles here */
-  </style>
+/* Add your styles here */
+</style>
   
+
---
+++ src/components/FlowSidebar.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <h2 :class="textStyle">Welcome to Vahalah</h2>
+  </div>
+  <input v-model="message" placeholder="Enter message" />
+  <p>Message is: {{ message }}</p>
+  <select v-model="selected">
+    <option disabled value="">Choose a fruit</option>
+    <option>Orange</option>
+    <option>Banana</option>
+    <option>Orange</option>
+  </select>
+  <span>Fruit chosen: {{ selected }}</span>
+  <a v-bind:ref="'https://' + domain + path">{{ linkText }}</a>
+
+  <!-- Watchers -->
+  <div>
+    <p>My name is: {{ name }}</p>
+    <button @click="nameChange()">Change Name</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+      default: {
+        firstName: "John",
+        lastName: "",
+      },
+    },
+  },
+  data() {
+    return {
+      isUnderline: true,
+      isBold: true,
+      name: "Flavio",
+    };
+  },
+  computed: {
+    textStyle: function () {
+      return {
+        underline: this.isUnderline,
+        bold: this.isBold,
+      };
+    },
+  },
+  methods: {
+    nameChange: function () {
+      this.name = "Flavius";
+    },
+  },
+  watch: {
+    name: function () {
+      console.log(this.name);
+    },
+  },
+};
+</script>
+
+<style>
+.underline {
+  text-decoration: underline;
+}
+.bold {
+  font-weight: bold;
+}
+</style>
